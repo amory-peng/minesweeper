e55f240ae52c96ea1e54b856a922a88ba8174f01
nickcianci <n.cianci3@gmail.com>
2016-11-17 15:48:38 -0800

    added save game function

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,11 +1,18 @@
+require 'yaml'
 require_relative "Board"
 
 class Minesweeper
 
+  def self.load_game_from_file(filename)
+    board_text = File.read(filename)
+    loaded_board = YAML::load(board_text)
+    new(loaded_board).play
+  end
+
   attr_accessor :board, :game_over
 
-  def initialize
-    @board = Board.new
+  def initialize(board = Board.new)
+    @board = board
     @game_over = false
   end
 
@@ -23,19 +30,24 @@ class Minesweeper
 
   def prompt_player
     puts "pick a position:"
-    parse_pos(gets.chomp)
+    parse_pos($stdin.gets.chomp)
   end
 
   def prompt_action(pos)
-    puts "R or F"
-    input = gets.chomp.upcase
+    puts "R, S, F"
+    input = $stdin.gets.chomp.upcase
     if input == "R"
       board.reveal(pos)
+    elsif input == "S"
+      save_game
     else
       board.flag(pos)
     end
   end
 
+  def save_game
+    File.open("game01.txt", 'w') { |file| file.write(board.to_yaml) }
+  end
 
   def parse_pos(input)
     input.split(",").map {|char| Integer(char)}
@@ -60,4 +72,10 @@ class Minesweeper
   end
 end
 
-Minesweeper.new.play
+if __FILE__ == $PROGRAM_NAME
+  if ARGV.empty?
+    Minesweeper.new.play
+  else
+    Minesweeper.load_game_from_file(ARGV.first)
+  end
+end
